5d947bf3e493117afceda6e85ef9b4b2323c7a0d
cptharris <67209324+cptharris@users.noreply.github.com>
2023-08-17 09:36:44 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Caleb Harris on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
