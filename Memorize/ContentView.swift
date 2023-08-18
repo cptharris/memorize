d783a83a2f4d62394d775badf9256c0b0d5a770a
cptharris <67209324+cptharris@users.noreply.github.com>
2023-08-18 09:32:42 -0700

    Create CardView Show several cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,17 +7,54 @@
 
 import SwiftUI
 
+// ": View" means "behaves like a View"
 struct ContentView: View {
+    // the body is a property of the View
+    // this one is a Computer Property
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(Color.purple)
         .padding()
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 20)
+                    .strokeBorder(lineWidth: 5)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 20)
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
