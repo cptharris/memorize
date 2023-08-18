d0965761b8161a3aa20808b4acc11e4491bb644b
cptharris <67209324+cptharris@users.noreply.github.com>
2023-08-18 11:13:16 -0700

    Change spaces to tabs Remove redundancy

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,99 +9,82 @@ import SwiftUI
 
 // ": View" means "behaves like a View"
 struct ContentView: View {
-    // ': Array<String>' or '[String]'
-    let emojis = ["👻", "😈", "🎃", "🕷️", "💀", "🕸️", "🙀", "👹", "🧌"]
-    @State var cardCount = 4
-
-    var body: some View {
-        VStack {
+	// ': Array<String>' or '[String]'
+	let emojis = ["👻", "😈", "🎃", "🕷️", "💀", "🕸️", "🙀", "👹", "🧌"]
+	@State var cardCount = 4
+	
+	var body: some View {
+		VStack {
 			ScrollView {
-				cards.foregroundColor(.purple)
+				cards
+					.foregroundColor(.purple)
 			}
+			
 			Spacer()
-            cardCountAdjust
-            .imageScale(.large)
-            .font(.largeTitle)
-            .foregroundColor(.accentColor)
-            .padding()
-        }
-        .padding()
-    }
-
-    var cards: some View {
-		LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
-            ForEach(0..<cardCount, id: \.self) { index in
-                CardView(content: emojis[index])
+			Rectangle()
+				.frame(height: 1)
+				.edgesIgnoringSafeArea(.horizontal)
+			
+			cardCountAdjust
+				.imageScale(.large)
+				.font(.largeTitle)
+				.foregroundColor(.accentColor)
+				.padding()
+		}
+		.padding()
+	}
+	
+	var cards: some View {
+		LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]) {
+			ForEach(0..<cardCount, id: \.self) { index in
+				CardView(content: emojis[index])
 					.aspectRatio(2/3, contentMode: .fit)
-            }
-        }
-    }
-
-    var cardCountAdjust: some View {
-        HStack {
-			cardRemover
-            Spacer()
-            cardAdder
-        }
-    }
+			}
+		}
+	}
+	
+	var cardCountAdjust: some View {
+		HStack {
+			cardCountAdjuster(by: -1, symbol: "minus")
+			Spacer()
+			cardCountAdjuster(by: +1, symbol: "plus")
+		}
+	}
 	
 	func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
 		Button(action: {
 			cardCount += offset
 		}, label: {
-			Image(systemName: symbol + ".square")
+			Image(systemName: "\(symbol).square")
 		})
 		.disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
 	}
-
-    var cardRemover: some View {
-        return cardCountAdjuster(by: -1, symbol: "minus")
-    }
-
-    var cardAdder: some View {
-        return cardCountAdjuster(by: +1, symbol: "plus")
-    }
 }
 
 struct CardView: View {
-    let content: String
-    @State var isFaceUp = true
-
-    var body: some View {
-        // use let because variables in ViewBuilder do not vary
-        let base = RoundedRectangle(cornerRadius: 20)
-        ZStack {
-            Group {
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 5)
-                Text(content).font(.largeTitle)
-            }
+	let content: String
+	@State var isFaceUp = true
+	
+	var body: some View {
+		// use let because variables in ViewBuilder do not vary
+		let base = RoundedRectangle(cornerRadius: 20)
+		ZStack {
+			Group {
+				base.fill(.white)
+				base.strokeBorder(lineWidth: 5)
+				Text(content).font(.largeTitle)
+			}
 			.opacity(isFaceUp ? 1 : 0)
 			base.fill().opacity(isFaceUp ? 0 : 1)
-        }
-        .onTapGesture {
-            isFaceUp.toggle()
-        }
-    }
+		}
+		.onTapGesture {
+			isFaceUp.toggle()
+		}
+	}
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
