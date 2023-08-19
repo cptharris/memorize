8653760ae9d43310327291f3b52c5e3c03fe9596
cptharris <67209324+cptharris@users.noreply.github.com>
2023-08-18 19:20:38 -0700

    Add more emoji sets Remove card count buttons Display 8 random card pairs

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,60 +10,89 @@ import SwiftUI
 // ": View" means "behaves like a View"
 struct ContentView: View {
 	// ': Array<String>' or '[String]'
-	let emojis = ["👻", "😈", "🎃", "🕷️", "💀", "🕸️", "🙀", "👹", "🧌"]
-	@State var cardCount = 4
+	let emojis = [
+		[["halloween", "🎃"], ["👻", "😈", "🎃", "🕷️", "💀", "🕸️", "🙀", "👹", "🧌", "🧟"]],
+		[["christmas", "🎁"], ["🎁", "🎄", "🎅", "🧝", "🕯️", "❄️", "⛄️", "🦌", "🛷", "🍪"]],
+		[["animals", "🐰"], ["🐰", "🐶", "🐦", "🐧", "🦎", "🐱", "🐴", "🐟", "🐠", "🦜"]]
+	]
+	let themeColors = [Color.orange, Color.blue, Color.green]
+	@State var currentSet = ["👻", "🕸️", "🧟", "🕷️", "🧌", "🧌", "🕸️", "💀", "👹", "👻", "👹", "🧟", "😈", "😈", "🕷️", "💀"]
+	@State var emojiSet = -1
 	
 	var body: some View {
 		VStack {
-			ScrollView {
-				cards
-					.foregroundColor(.purple)
-			}
-			
-			Spacer()
-			Rectangle()
-				.frame(height: 1)
-				.edgesIgnoringSafeArea(.horizontal)
-			
-			cardCountAdjust
-				.imageScale(.large)
+			Text("Memorize!")
 				.font(.largeTitle)
-				.foregroundColor(.accentColor)
-				.padding()
+			
+			if emojiSet != -1 {
+				ScrollView {
+					cards
+						.foregroundColor(themeColors[emojiSet])
+				}
+			} else {
+				emojiSelect
+					.imageScale(.large)
+					.font(.largeTitle)
+					.foregroundColor(.accentColor)
+			}
 		}
-		.padding()
+		.padding(10)
+	}
+	
+	var emojiSelect: some View {
+		HStack {
+			ForEach(0..<emojis.count, id: \.self) { index in
+				Button(action: {
+					emojiSet = index
+					// get the selected emoji set and shuffle them
+					currentSet = shuffledSet()
+				}, label: {
+					VStack {
+						Text(emojis[index][0][1])
+							.font(.title)
+						Text(emojis[index][0][0])
+							.font(.caption2)
+					}
+				})
+			}
+		}
+	}
+	
+	func shuffledSet() -> [String] {
+		var newSet = emojis[emojiSet][1].shuffled()
+		while newSet.count > 8 {
+			newSet.removeFirst()
+		}
+		return (newSet + newSet).shuffled()
 	}
 	
 	var cards: some View {
-		LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]) {
-			ForEach(0..<cardCount, id: \.self) { index in
-				CardView(content: emojis[index])
+		LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))]) {
+			ForEach(0..<currentSet.count, id: \.self) { index in
+				CardView(content: currentSet[index])
 					.aspectRatio(2/3, contentMode: .fit)
 			}
 		}
 	}
-	
-	var cardCountAdjust: some View {
-		HStack {
-			cardCountAdjuster(by: -1, symbol: "minus")
-			Spacer()
-			cardCountAdjuster(by: +1, symbol: "plus")
-		}
-	}
-	
-	func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
-		Button(action: {
-			cardCount += offset
-		}, label: {
-			Image(systemName: "\(symbol).square")
-		})
-		.disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
-	}
 }
+
+//class theme {
+//	var name: String
+//	var symbol: String
+//	var color: Color = .accentColor
+//	var emojiSet: [String]
+//
+//	init(name: String, symbol: String, color: Color, emojiSet: [String]) {
+//		self.name = name
+//		self.symbol = symbol
+//		self.color = color
+//		self.emojiSet = emojiSet
+//	}
+//}
 
 struct CardView: View {
 	let content: String
-	@State var isFaceUp = true
+	@State var isFaceUp = false
 	
 	var body: some View {
 		// use let because variables in ViewBuilder do not vary
