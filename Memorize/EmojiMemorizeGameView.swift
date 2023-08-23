3316d2c58683277edab6f215cb31394118127b80
cptharris <67209324+cptharris@users.noreply.github.com>
2023-08-23 11:18:16 -0700

    Automatically fit cards to the alloted space

--- Memorize/EmojiMemorizeGameView.swift
+++ Memorize/EmojiMemorizeGameView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct EmojiMemorizeGameView: View {
 	@ObservedObject var gameKeeper: EmojiMemorizeGame
 	
+	private let cardAspectRatio: CGFloat = 2/3
+	
 	var body: some View {
 		VStack {
 			Text("Memorize!")
@@ -28,7 +30,7 @@ struct EmojiMemorizeGameView: View {
 		.padding(10)
 	}
 	
-	var lowerPanelView: some View {
+	private var lowerPanelView: some View {
 		HStack {
 			Text("Score: \(gameKeeper.gameScore)").opacity(0)
 			Spacer()
@@ -40,12 +42,17 @@ struct EmojiMemorizeGameView: View {
 		}
 	}
 	
-	var cardsView: some View {
-		ScrollView {
-			LazyVGrid(columns: [GridItem(.adaptive(minimum: 80), spacing: 0)], spacing: 0) {
+	private var cardsView: some View {
+		GeometryReader { geometry in
+			let gridItemSize = gridItemWidthThatFits(
+				count: gameKeeper.getCards.count,
+				size: geometry.size,
+				atAspectRatio: cardAspectRatio)
+			
+			LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
 				ForEach(gameKeeper.getCards) { card in
 					CardView(card)
-						.aspectRatio(2/3, contentMode: .fit)
+						.aspectRatio(cardAspectRatio, contentMode: .fit)
 						.padding(2)
 						.onTapGesture {
 							gameKeeper.choose(card)
@@ -54,10 +61,30 @@ struct EmojiMemorizeGameView: View {
 			}
 		}
 	}
+	
+	private func gridItemWidthThatFits(count: Int, size: CGSize, atAspectRatio aspectRatio: CGFloat) -> CGFloat {
+		let count = CGFloat(count)
+		var columnCount = 1.0
+		repeat {
+			// width and height of every card, given this number of columns
+			let width = size.width / columnCount
+			let height = width / aspectRatio
+			// number of cards divided by number of columns give number of rows
+			let rowCount = (count / columnCount).rounded(.up)
+			
+			// if the total height fits in the alloted space, return the width
+			if rowCount * height < size.height {
+				return (width).rounded(.down)
+			}
+			// if the total height does not fit, add another column
+			columnCount += 1
+		} while columnCount < count
+		return min(size.width / Double(count), size.height * aspectRatio).rounded(.down)
+	}
 }
 
 /// displays a card
-struct CardView: View {
+private struct CardView: View {
 	let card: MemorizeGame<String>.Card
 	
 	init(_ card: MemorizeGame<String>.Card) {
