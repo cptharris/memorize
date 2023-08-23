a77dec68bd477a453225138885332dac49fbb811
cptharris <67209324+cptharris@users.noreply.github.com>
2023-08-23 11:49:23 -0700

    Create AspectVGrid

--- Memorize/AspectVGrid.swift
+++ Memorize/AspectVGrid.swift
@@ -7,14 +7,50 @@
 
 import SwiftUI
 
-struct AspectVGrid: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct AspectVGrid_Previews: PreviewProvider {
-    static var previews: some View {
-        AspectVGrid()
-    }
+struct AspectVGrid<Item: Identifiable, ItemView: View>: View {
+	private var items: [Item]
+	private var aspectRatio: CGFloat = 1
+	private var content: (Item) -> ItemView
+	
+	init(_ items: [Item], aspectRatio: CGFloat, @ViewBuilder content: @escaping (Item) -> ItemView) {
+		self.items = items
+		self.aspectRatio = aspectRatio
+		self.content = content
+	}
+	
+	var body: some View {
+		GeometryReader { geometry in
+			let gridItemSize = gridItemWidthThatFits(
+				count: items.count,
+				size: geometry.size,
+				atAspectRatio: aspectRatio
+			)
+			
+			LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
+				ForEach(items) { item in
+					content(item).aspectRatio(aspectRatio, contentMode: .fit)
+				}
+			}
+		}
+	}
+	
+	private func gridItemWidthThatFits(count: Int, size: CGSize, atAspectRatio aspectRatio: CGFloat) -> CGFloat {
+		let count = CGFloat(count)
+		var columnCount = 1.0
+		repeat {
+			// width and height of every card, given this number of columns
+			let width = size.width / columnCount
+			let height = width / aspectRatio
+			// number of cards divided by number of columns give number of rows
+			let rowCount = (count / columnCount).rounded(.up)
+			
+			// if the total height fits in the alloted space, return the width
+			if rowCount * height < size.height {
+				return (width).rounded(.down)
+			}
+			// if the total height does not fit, add another column
+			columnCount += 1
+		} while columnCount < count
+		return min(size.width / Double(count), size.height * aspectRatio).rounded(.down)
+	}
 }
--- Memorize/EmojiMemorizeGameView.swift
+++ Memorize/EmojiMemorizeGameView.swift
@@ -43,43 +43,13 @@ struct EmojiMemorizeGameView: View {
 	}
 	
 	private var cardsView: some View {
-		GeometryReader { geometry in
-			let gridItemSize = gridItemWidthThatFits(
-				count: gameKeeper.getCards.count,
-				size: geometry.size,
-				atAspectRatio: cardAspectRatio)
-			
-			LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
-				ForEach(gameKeeper.getCards) { card in
-					CardView(card)
-						.aspectRatio(cardAspectRatio, contentMode: .fit)
-						.padding(2)
-						.onTapGesture {
-							gameKeeper.choose(card)
-						}
+		AspectVGrid(gameKeeper.getCards, aspectRatio: cardAspectRatio) { card in
+			CardView(card)
+				.padding(2)
+				.onTapGesture {
+					gameKeeper.choose(card)
 				}
-			}
 		}
-	}
-	
-	private func gridItemWidthThatFits(count: Int, size: CGSize, atAspectRatio aspectRatio: CGFloat) -> CGFloat {
-		let count = CGFloat(count)
-		var columnCount = 1.0
-		repeat {
-			// width and height of every card, given this number of columns
-			let width = size.width / columnCount
-			let height = width / aspectRatio
-			// number of cards divided by number of columns give number of rows
-			let rowCount = (count / columnCount).rounded(.up)
-			
-			// if the total height fits in the alloted space, return the width
-			if rowCount * height < size.height {
-				return (width).rounded(.down)
-			}
-			// if the total height does not fit, add another column
-			columnCount += 1
-		} while columnCount < count
-		return min(size.width / Double(count), size.height * aspectRatio).rounded(.down)
 	}
 }
 
